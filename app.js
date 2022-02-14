9e4732db124153221c7ccfcf8d5e6e4ddd9c317b
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-02-14 22:55:19 +0600

    generating pin

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
-    const pinString = pin + " ";
+    const pinString = pin + "";
     if (pinString.length == 4) {
         return pin;
     } else {
-        console.log("got 3 digit and calling again");
+        // console.log("got 3 digit and calling again");
         return getPin();
     }
 }
