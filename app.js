7125da5b50ea945e8c6489f9ae3c719a8d23044c
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-02-15 08:52:32 +0600

    matching pin

--- app.js
+++ app.js
@@ -15,3 +15,33 @@ function generatePin() {
 
     // console.log(pin);
 }
+
+document.getElementById("keypad").addEventListener("click", function(event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById("typed-numbers");
+
+    if (isNaN(number)) {
+        if (number == "C") {
+            calcInput.value = "";
+        }
+    } else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
+
+function verifyPin() {
+    const pin = document.getElementById("display-pin").value;
+    const typeNumbers = document.getElementById("typed-numbers").value;
+    const sucessMessage = document.getElementById("notify-sucess");
+    const failError = document.getElementById("notify-fail");
+
+    if (pin == typeNumbers) {
+        sucessMessage.style.display = "block";
+        failError.style.display = "none";
+    } else {
+        failError.style.display = "block";
+        sucessMessage.style.display = "none";
+    }
+}
